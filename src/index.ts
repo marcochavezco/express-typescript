0f190ef1673e0712466f0304e981be733b3c9961
marcochavezco <machavezco@gmail.com>
2024-03-19 22:11:42 -0600

    Add router and wip diary services

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+
+import diaryRouter from './routes/diaries';
+
 const app = express();
 app.use(express.json());
 
@@ -9,6 +12,7 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+app.use('/api/diaries', diaryRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
